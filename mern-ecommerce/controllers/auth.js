8ed089ce76712eee9570ca989ba8a406f9cc3d8f
SyedArshikJavedMoinee <arshik.javed@gmail.com>
2024-06-12 12:15:09 +0500

    First Commit

--- mern-ecommerce/controllers/auth.js
+++ mern-ecommerce/controllers/auth.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 exports.signup = (req, res) => {
   // console.log('req.body', req.body);
-  const user = new User(req.body);
+  const user = new Userr(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
