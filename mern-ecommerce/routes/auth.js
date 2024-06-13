c76e94e6de5c76a473ff395183e2c216ef234164
SyedArshikJavedMoinee <arshik.javed@gmail.com>
2024-06-13 16:11:41 +0500

    First one for demo

--- mern-ecommerce/routes/auth.js
+++ mern-ecommerce/routes/auth.js
@@ -12,5 +12,8 @@ const { userSignupValidator } = require('../validator');
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+router.get('/hello', requireSignin, (req, res) => {
+  res.send('hello there');
+});
 
 module.exports = router;
