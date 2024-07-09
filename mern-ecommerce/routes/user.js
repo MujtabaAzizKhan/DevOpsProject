a708f13484e2de1cce849506594249b3dd9ae1e1
SyedArshikJavedMoinee <arshik.javed@gmail.com>
2024-07-09 11:25:38 +0500

    testing

--- mern-ecommerce/routes/user.js
+++ mern-ecommerce/routes/user.js
@@ -18,7 +18,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
-router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistoryy);
 
 router.param('userId', userById);
 
