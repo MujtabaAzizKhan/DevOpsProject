6e56c6806f8033e52e8be86f4a76007d2d7d8ffe
SyedArshikJavedMoinee <arshik.javed@gmail.com>
2024-07-09 16:16:44 +0500

    testing

--- mern-ecommerce/controllers/order.js
+++ mern-ecommerce/controllers/order.js
@@ -62,14 +62,14 @@ exports.updateOrderStatus = (req, res) => {
   );
 };
 
-exports.removeOrder = async (req, res) => {
+exports.deleteOrder = async (req, res) => {
   try {
-    const deletionResult = await Order.findOneAndDelete({ _id: req.params.orderId });
-    if (!deletionResult) {
-      return res.status(404).json({ error: "Order not found" });
+    const order = await Order.findByIdAndRemove(req.params.orderId);
+    if (!order) {
+      return res.status(404).json({ message: "Order does not exist" });
     }
-    res.json({ message: "Order removed successfully" });
-  } catch (error) {
-    res.status(500).json({ error: errorHandler(error) });
+    res.status(200).json({ message: "Order deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: errorHandler(err) });
   }
 };
