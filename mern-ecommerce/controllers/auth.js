bdafbad86641648ad8cad91a6e1de67f18b9dba6
SyedArshikJavedMoinee <arshik.javed@gmail.com>
2024-07-09 10:39:32 +0500

    testing

--- mern-ecommerce/controllers/auth.js
+++ mern-ecommerce/controllers/auth.js
@@ -62,20 +62,20 @@ exports.requireSignin = expressJwt({
   userProperty: 'auth',
 });
 
-exports.isAuth = (req, res, next) => {
-  let user = req.profile && req.auth && req.profile._id == req.auth._id;
-  if (!user) {
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
     return res.status(403).json({
-      error: 'Access denied',
+      error: 'Admin resource! Access denied',
     });
   }
   next();
 };
 
-exports.isAdmin = (req, res, next) => {
-  if (req.profile.role === 0) {
+exports.isAuth = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!user) {
     return res.status(403).json({
-      error: 'Admin resource! Access denied',
+      error: 'Access denied',
     });
   }
   next();
